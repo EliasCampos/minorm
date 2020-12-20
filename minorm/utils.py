c2879927cc767c0002c7c8927e8af9c413161186
Ilya Campos <campos.ilya@steelkiwi.com>
2020-12-20 05:33:37 +0200

    remove unused utils module

--- minorm/utils.py
+++
@@ -1,8 +0,0 @@
-from minorm.connectors import SQLiteDatabase
-
-
-def pk_declaration_for_db(db):
-    if isinstance(db, SQLiteDatabase):
-        return 'INTEGER PRIMARY KEY AUTOINCREMENT'
-
-    raise ValueError('Unsupported DB type.')

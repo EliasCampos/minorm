6946a7fb83702b5c937e1315c41067c62fd48dee
Ilya Campos <campos.ilya@steelkiwi.com>
2021-01-03 00:33:42 +0200

    set pre-alpha

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     author_email='camposylia@gmail.com',
     license="MIT",
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
